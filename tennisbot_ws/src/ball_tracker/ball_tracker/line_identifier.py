838557fbe475ff0da113b261aa0c711eac566efd
zhizhuo17 <zzha0170@student.monash.edu>
2024-09-16 23:04:20 +1000

    fixed ball_in_out.py

--- tennisbot_ws/src/ball_tracker/ball_tracker/line_identifier.py
+++ tennisbot_ws/src/ball_tracker/ball_tracker/line_identifier.py
@@ -24,13 +24,30 @@ def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
     return cv2.addWeighted(initial_img, α, img, β, λ)
 
 
-test_image_folder = 'tennisbot_ws/src/ball_tracker/ball_tracker/test_images_court'
+def get_line_equations(houghLines):
+    if houghLines is None:
+        return None
+    
+    line_equations = []
+    for line in houghLines:
+        for rho, theta in line:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            
+            # Check if the line is vertical (theta close to 0 or pi)
+            if b == 0:  # Vertical line case
+                x_intercept = rho / a
+                line_equations.append(f"x = {x_intercept}")
+            else:
+                # For non-vertical lines, calculate slope (m) and y-intercept (c)
+                m = -a / b
+                c = rho / b
+                line_equations.append(f"y = {m}x + {c}")
+    
+    return line_equations
 
-test_images = [f for f in listdir(test_image_folder) if isfile(join(test_image_folder, f))]
-
-for image_path in test_images:
-    image = cv2.cvtColor(cv2.imread(f'{test_image_folder}/{image_path}'), cv2.COLOR_BGR2RGB)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+def find_lines(img): 
+    gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     blurred_image = cv2.GaussianBlur(gray_image, (9, 9), 2)
     edges_image = cv2.Canny(blurred_image, 50, 120)
     
@@ -40,15 +57,24 @@ for image_path in test_images:
     
     hough_lines = cv2.HoughLines(edges_image, rho_resolution , theta_resolution , threshold)
     
-    hough_lines_image = np.zeros_like(image)
+    if hough_lines is None:
+        return []
+
+
+    hough_lines_image = np.zeros_like(img)
     draw_lines(hough_lines_image, hough_lines)
-    original_image_with_hough_lines = weighted_img(hough_lines_image,image)
+    original_image_with_hough_lines = weighted_img(hough_lines_image,img)
+
+    return original_image_with_hough_lines
+
+
+
+test_image_folder = 'tennisbot_ws/src/ball_tracker/ball_tracker/test_images_real'
+test_images = [f for f in listdir(test_image_folder) if isfile(join(test_image_folder, f))]
+
+for image_path in test_images:
+    image = cv2.cvtColor(cv2.imread(f'{test_image_folder}/{image_path}'), cv2.COLOR_BGR2RGB)
+    image = find_lines(image)
     
-    plt.figure(figsize = (30,20))
-    plt.subplot(131)
-    plt.imshow(image)
-    plt.subplot(132)
-    plt.imshow(edges_image, cmap='gray')
-    plt.subplot(133)
-    plt.imshow(original_image_with_hough_lines, cmap='gray') 
+    plt.imshow(image, cmap='gray') 
     plt.show()
