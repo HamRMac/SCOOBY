c3b6366b0e3b07f49d4159ee5ec103e514734e51
TennisBotPi <E14Tennisbot@MonashBots.bots>
2024-10-03 12:00:03 +1000

    Modified start camera to also start rotated view

--- tennisbot_ws/src/tennisbotmaster/launch/start_camera.launch.py
+++ tennisbot_ws/src/tennisbotmaster/launch/start_camera.launch.py
@@ -13,16 +13,28 @@ import xacro
 def generate_launch_description():
 
     # Create a robot_state_publisher node
-    params = {'image_size': [640,480], 'camera_frame_id': "camera_optical_link"}
+    params_camera = {'image_size': [640, 480], 'camera_frame_id': "camera_optical_link"}
     node_camera_pub = Node(
         package='v4l2_camera',
         executable='v4l2_camera_node',
         output='screen',
-        parameters=[params]
+        parameters=[params_camera]
     )
 
+    # Add the image_rotator node
+    params_image_rotator = {
+        'input_topic': '/image_raw',
+        'output_topic': '/camera/rotated_image'
+    }
+    node_image_rotator = Node(
+        package='image_rotator',
+        executable='image_rotator_node',
+        output='screen',
+        parameters=[params_image_rotator]
+    )
 
     # Launch!
     return LaunchDescription([
-        node_camera_pub
+        node_camera_pub,
+        node_image_rotator
     ])
